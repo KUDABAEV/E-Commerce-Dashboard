bd52a982e28e167d9e12f5c5f0db0f1541626df5
Arthur <arturo.kudabaev@mail.ru>
2023-11-28 22:43:27 +0500

    lesson-4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import {Route, Routes} from "react-router-dom";
 import Home from "./components/home";
+import Login from "./components/auth/login";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Home page</h1>
-        <Home />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='login' element={<Login/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
